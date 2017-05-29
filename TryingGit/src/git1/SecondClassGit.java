f1be0578a10908a87acf364122e7e6fd865f8205
Pranitha <Pranitha@192.168.2.2>
2017-05-29 21:10:52 +0530

    secnd script

---
+++ TryingGit/src/git1/SecondClassGit.java
@@ -0,0 +1,13 @@
+package git1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SecondClassGit {
+	@Test
+	public void testX()
+	{
+		Reporter.log("hi",true);
+	}
+
+}

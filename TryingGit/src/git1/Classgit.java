303ca1e5afe297d4920292ad0fd5a83d10b467ed
Pranitha <Pranitha@192.168.2.2>
2017-05-29 20:59:14 +0530

    uploading automatn framework

---
+++ TryingGit/src/git1/Classgit.java
@@ -0,0 +1,13 @@
+package git1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Classgit {
+	@Test
+	public void testgit()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
